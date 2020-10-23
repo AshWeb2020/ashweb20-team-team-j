e917414fd9b811feb4e8d5cf2e9175daadfa0db7
Abdul-Fuseni <abdul.fuseini@ashesi.edu.gh>
2020-10-23 21:12:18 +0000

    backend

--- Leo.php
+++ Leo.php
@@ -1,14 +1,15 @@
 <?php 
 	if (isset($_POST['submit'])) {
 
-		$Fname = $_POST['First Name'];
-		$Lname = $_POST['Last Name'];
-		$Sex = $_POST['Gender'];
-		$Mail = $_POST['Email'];
-		$Pssword = $_POST['Password'];
-		$Phone = $_POST['Phone Number'];
-		$Year = $_POST['Year Group'];
-		$Course = $_POST['Course'];
+		$Fname = $_POST['first'];
+		$Lname = $_POST['last'];
+		$Sex = $_POST['gender'];
+		$Mail = $_POST['mail'];
+		$Password = $_POST['password'];
+		$Cpassword = $_POST['Cpassword'];
+		$Phone = $_POST['phone'];
+		$Year = $_POST['year'];
+		$Course = $_POST['course'];
 
 		$conn = mysqli_connect('localhost', 'root', 'root', 'sprint1');
 
@@ -17,5 +18,7 @@
     		die("ERROR: Could not connect. " . mysqli_connect_error());}
 		}
 
-		
+		$sql = "INSERT INTO newMembership(first_name, last_name, gender, email, pssword, confirm_pssword, phonenumber, yearGroup, course) VALUES ('$Fname', '$Lname', 	'$Sex', '$Mail', '$Password', '$Cpassword', '$Phone', '$Year', '$Course')";
+
+		mysqli_query($conn, $sql);
  ?>
--- sprint1.sql
+++ sprint1.sql
@@ -2,14 +2,15 @@ create database sprint1;
 use sprint1;
 
 create table newMembership(
-person_id mediumint unsigned auto_increment primary key,
 first_name varchar(40),
 last_name varchar(40),
+gender varchar(60),
 email varchar(50),
+pssword varchar(60),
+confirm_pssword varchar(60),
 phonenumber varchar(25),
-gender enum('m','f'),
-yearGroup enum("2021", "2022","2023","2024"),
-course enum("Business Adminstration","Management Information System","Computer science","Mechanical Engineering","Electrical and Electronic Engineering","Computer Engineering")
+yearGroup varchar(100),
+course varchar(100)
 );
 
 create table activity(
